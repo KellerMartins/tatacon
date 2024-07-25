22689c1034cf949d9286c26ed52b410c2f7d3626
Keller Clayderman <keller.clayderman.m@gmail.com>
2024-07-25 00:04:22 -0300

    Many changes, including key layout, formatting and button press behavior

--- tatacon.ino
+++ tatacon.ino
@@ -1,7 +1,7 @@
-#include <limits.h>
 #include <HID-Project.h>
 #include <HID-Settings.h>
 #include <Keypad.h>
+#include <limits.h>
 
 bool on = false;
 bool debug_mode = false;
@@ -9,23 +9,24 @@ bool debug_mode = false;
 unsigned long int lastTime;
 
 #define CHANNELS 4
-int channelSample [CHANNELS];
-int lastChannelSample [CHANNELS];
+int channelSample[CHANNELS];
+int lastChannelSample[CHANNELS];
 
-long int diff [CHANNELS];
-bool triggered [CHANNELS];
-unsigned int releaseTick [CHANNELS];
+long int diff[CHANNELS];
+bool triggered[CHANNELS];
+unsigned int releaseTick[CHANNELS];
 
 // ---- Drum pins and settings ----
 
 const int RXLED = 17;
 
-const int DRUM_PINS[] = {A0, A1, A2, A3};  // L don, R don, L kat, R kat
-const char DRUM_KEYS[] = {'d', 'f', 'j', 'k'};
-const int DRUM_SENSITIVITY[] = {2, 1, 1, 3};
+const int DRUM_PINS[] = {A0, A1, A2, A3}; // L don, R don, L kat, R kat
+const char DRUM_KEYS[] = {KEY_UP_ARROW, KEY_RIGHT_ARROW, KEY_V, KEY_C};
+const int DRUM_SENSITIVITY[] = {2, 1, 1, 2};
 
-const int THRESHOLD = 60;
-const int DEBOUNCE_TICKS  = 60;
+const int THRESHOLD = 40;
+const int HOLD_TICKS = 30;
+const int DEBOUNCE_TICKS = 10;
 
 // ---- Keypad pins and mapping ----
 const byte ROWS = 3;
@@ -38,134 +39,124 @@ const byte VOLUME_DOWN_BUTTON = ']';
 
 // Keypad button mapping
 const char KEYS[ROWS][COLS] = {
-  {KEY_RETURN,KEY_RIGHT_ARROW,KEY_UP_ARROW,KEY_LEFT_CTRL},
-  {KEY_ESC,KEY_LEFT_ARROW,KEY_DOWN_ARROW,KEY_F2},
-  {ON_OFF_BUTTON, NO_KEY, VOLUME_UP_BUTTON, VOLUME_DOWN_BUTTON},
+    {KEY_Z, KEY_RIGHT_ARROW, KEY_UP_ARROW, KEY_M},
+    {KEY_X, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_Z},
+    {ON_OFF_BUTTON, NO_KEY, VOLUME_UP_BUTTON, VOLUME_DOWN_BUTTON},
 };
 
-const byte ROW_PINS[ROWS] = {7, 6, 4}; //connect to the row pinouts of the keypad
-const byte COL_PINS[COLS] = {3, 2, 1, 0}; //connect to the column pinouts of the keypad
+const byte ROW_PINS[ROWS] = {7, 6, 4};    // connect to the row pinouts of the keypad
+const byte COL_PINS[COLS] = {3, 2, 1, 0}; // connect to the column pinouts of the keypad
 
-Keypad keypad = Keypad( makeKeymap(KEYS), ROW_PINS, COL_PINS, ROWS, COLS );
+Keypad keypad = Keypad(makeKeymap(KEYS), ROW_PINS, COL_PINS, ROWS, COLS);
 // ----------------
 
 void setup() {
-  Keyboard.begin ();
-  analogReference (DEFAULT);
-  
-  // Change ADC prescaler to speed up analogRead
-  // https://gammon.com.au/adc
-  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits 
-  ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 
-  
-  for (short int i = 0; i < CHANNELS; i++) {
-    lastChannelSample[i] = 0;
-    releaseTick[i] = DEBOUNCE_TICKS;
-    triggered[i] = false;
-  }
-  lastTime = 0;
+    Keyboard.begin();
+    analogReference(DEFAULT);
 
-  pinMode(RXLED, OUTPUT);
-  digitalWrite(RXLED, LOW);
-  Consumer.begin();
+    // Change ADC prescaler to speed up analogRead
+    // https://gammon.com.au/adc
+    ADCSRA &= ~(bit(ADPS0) | bit(ADPS1) | bit(ADPS2)); // clear prescaler bits
+    ADCSRA |= bit(ADPS1) | bit(ADPS2);                 //  64
+
+    for (short int i = 0; i < CHANNELS; i++) {
+        lastChannelSample[i] = 0;
+        releaseTick[i] = DEBOUNCE_TICKS;
+        triggered[i] = false;
+    }
+    lastTime = 0;
+
+    pinMode(RXLED, OUTPUT);
+    digitalWrite(RXLED, LOW);
+    Consumer.begin();
 }
 
 void loop() {
 
-  for (short int i = 0; i < CHANNELS; i++) {
-    lastChannelSample[i] = channelSample[i];
-    channelSample[i] = analogRead(DRUM_PINS[i]) / DRUM_SENSITIVITY[i];
-    diff[i] = abs(channelSample[i] - lastChannelSample[i]);
-  }
-
-  bool don = releaseTick[1] != DEBOUNCE_TICKS || releaseTick[2] != DEBOUNCE_TICKS;
-  
-  for (short int i = 0; i < CHANNELS; i++) {
-    if (debug_mode){
-      Serial.print(diff[i]);
-      Serial.print("\t");
+    for (short int i = 0; i < CHANNELS; i++) {
+        lastChannelSample[i] = channelSample[i];
+        channelSample[i] = analogRead(DRUM_PINS[i]) / DRUM_SENSITIVITY[i];
+        diff[i] = abs(channelSample[i] - lastChannelSample[i]);
     }
-    
-    if (!on) continue;
-    
-    if (!triggered [i]) {
-      if (diff[i] >= THRESHOLD && releaseTick[i] == DEBOUNCE_TICKS && !(don && (i == 0 || i == 3))){
-       
-        triggered[i] = true;
-        Keyboard.press(DRUM_KEYS[i]);
-      } else {
-        releaseTick[i] = min(releaseTick[i]+1, DEBOUNCE_TICKS);
-      }
-    } else {
-      if (diff[i] < THRESHOLD && releaseTick[i] == DEBOUNCE_TICKS){
-        triggered[i] = false;
-        releaseTick[i] = 0;
-        Keyboard.release(DRUM_KEYS[i]);
-      }
+
+    for (short int i = 0; i < CHANNELS; i++) {
+        if (debug_mode) {
+            Serial.print(diff[i]);
+            Serial.print("\t");
+        }
+
+        if (!on)
+            continue;
+
+        if (!triggered[i]) {
+            if (diff[i] >= THRESHOLD && releaseTick[i] == HOLD_TICKS+DEBOUNCE_TICKS) {
+                triggered[i] = true;
+                releaseTick[i] = 0;
+                Keyboard.press(KeyboardKeycode(DRUM_KEYS[i]));
+            }
+        } else {
+            if (diff[i] < THRESHOLD && releaseTick[i] >= HOLD_TICKS) {
+                triggered[i] = false;
+                Keyboard.release(KeyboardKeycode(DRUM_KEYS[i]));
+            }
+        }
+
+        releaseTick[i] = min(releaseTick[i] + 1, HOLD_TICKS+DEBOUNCE_TICKS);
+        // End of each channel
     }
-  // End of each channel
-  }
 
-  if (debug_mode) {
-    Serial.print (THRESHOLD);
-    Serial.print ("\t");
-  }
+    if (debug_mode) {
+        Serial.print(THRESHOLD);
+        Serial.print("\t");
+    }
 
-  handleKeypad();
+    handleKeypad();
 
-  unsigned int frameTime = micros () - lastTime;
-  lastTime = micros ();
-  if (debug_mode) {
-    Serial.print ("(");
-    Serial.print(frameTime / 1000.0f);
-    Serial.print (")");
-  }
+    unsigned int frameTime = micros() - lastTime;
+    lastTime = micros();
+    if (debug_mode) {
+        Serial.print("(");
+        Serial.print(frameTime / 1000.0f);
+        Serial.print(")");
+    }
 
-  if (debug_mode) {
-    Serial.println ("");
-  }
+    if (debug_mode) {
+        Serial.println("");
+    }
 }
 
-void handleKeypad(){
-  char pressing = keypad.getKey() != NO_KEY;
-  Key key = keypad.key[0];
-  
-  switch(key.kchar) {
+void handleKeypad() {
+    char pressing = keypad.getKey() != NO_KEY;
+    Key key = keypad.key[0];
+
+    switch (key.kchar) {
     case NO_KEY:
-      break;
+        break;
     case (char)ON_OFF_BUTTON:
-      if (pressing){
-        on = !on;
-        digitalWrite(RXLED, on? LOW : HIGH);
-      } else if (key.kstate == HOLD && !debug_mode) {
-        debug_mode = true;
-        Serial.begin (115200);
-      }
-      break;
-    case (char)KEY_LEFT_ARROW:
-    case (char)KEY_RIGHT_ARROW:
-    case (char)KEY_UP_ARROW:
-    case (char)KEY_DOWN_ARROW:
-      if (pressing) {
-        Keyboard.press(KeyboardKeycode(key.kchar));
-      } else if (key.kstate == RELEASED) {
-        Keyboard.release(KeyboardKeycode(key.kchar));
-      }
-      break;
+        if (pressing) {
+            on = !on;
+            digitalWrite(RXLED, on ? LOW : HIGH);
+        } else if (key.kstate == HOLD && !debug_mode) {
+            debug_mode = true;
+            Serial.begin(115200);
+        }
+        break;
     case (char)VOLUME_UP_BUTTON:
-      if (pressing) {
-        Consumer.write(MEDIA_VOLUME_UP);
-      }
-      break;
+        if (pressing) {
+            Consumer.write(MEDIA_VOLUME_UP);
+        }
+        break;
     case (char)VOLUME_DOWN_BUTTON:
-      if (pressing) {
-        Consumer.write(MEDIA_VOLUME_DOWN);
-      }
-      break;
+        if (pressing) {
+            Consumer.write(MEDIA_VOLUME_DOWN);
+        }
+        break;
     default:
-      if (pressing) {
-        Keyboard.write(KeyboardKeycode(key.kchar));
-      }
-      break;
-  }
+        if (pressing) {
+            Keyboard.press(KeyboardKeycode(key.kchar));
+        } else if (key.kstate == RELEASED) {
+            Keyboard.release(KeyboardKeycode(key.kchar));
+        }
+        break;
+    }
 }
